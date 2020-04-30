131c1cd7a4eb12e62f8477a7b7f4297c3463f2a5
shiva <shivac995@gmail.com>
2020-04-30 13:00:35 -0400

    Add project files.

---
+++ todelapp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(todelapp.Startup))]
+namespace todelapp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
